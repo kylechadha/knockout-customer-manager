7c886e63f68940a9fbf7e822d75a2102c2c350dd
Kyle Chadha <kyle.chadha@gmail.com>
2015-01-17 16:15:19 -0500

    Update orders view and add currency binding

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,14 +1,37 @@
+ko.bindingHandlers.currency = {
+    symbol: ko.observable('$'),
+    update: function(element, valueAccessor, allBindingsAccessor){
+        return ko.bindingHandlers.text.update(element,function(){
+            var value = +(ko.utils.unwrapObservable(valueAccessor()) || 0),
+                symbol = ko.utils.unwrapObservable(allBindingsAccessor().symbol === undefined
+                            ? allBindingsAccessor().symbol
+                            : ko.bindingHandlers.currency.symbol);
+            return symbol + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+        });
+    }
+};
+
 function CustomerViewModel() {
     // Data
     var self = this;
-    self.customerData = ko.observable();
-    // self.chosenFolderId = ko.observable();
-    // self.chosenMailData = ko.observable();
+    self.customers = ko.observable();
+    self.orders = ko.observableArray();
+    self.customerOrder = ko.observable();
+    self.ordersTotal = ko.observable();
 
     // Behaviours    
     self.goToCustomers = function() { location.hash = 'customers' };
     self.goToOrders = function() { location.hash = 'orders' };
-    // self.goToMail = function(mail) { location.hash = mail.folder + '/' + mail.id };
+    self.goToOrder = function(order) { location.hash = 'orders/' + order.id}
+
+    var calculateTotal = function() {
+        var total = 0;
+        console.log(self.orders());
+        for (var i=0,len=self.orders().length;i<len;i++) {
+            total += self.orders()[i].total;
+        }
+        self.ordersTotal(total);
+    }
 
     // Client-side routes    
     Sammy(function() {
@@ -17,22 +40,45 @@ function CustomerViewModel() {
 
             $('.welcome').hide();
             $('.customers').show();
+            $('.orders').hide();
+            $('.customerOrder').hide();
 
-            $.get("/api/customers", self.customerData);
+            $.get("/api/customers", self.customers);
         });
 
         this.get('#orders', function() {
             console.log('Orders route');
+
+            $('.welcome').hide();
+            $('.customers').hide();
+            $('.orders').show();
+            $('.customerOrder').hide();
+
+            $.get("/api/orders", function(data) {
+                self.orders(data);
+                calculateTotal();
+            });
+
         });
 
         this.get('#orders/:customerId', function() {
             console.log('Customer order route');
+
+            $('.welcome').hide();
+            $('.customers').hide();
+            $('.orders').hide();
+            $('.customerOrder').show();
+
             // $.get("/customers", { folder: this.params.folder }, self.chosenFolderData);
         });
     
         this.get('', function() {
+
             $('.welcome').show();
             $('.customers').hide();
+            $('.orders').hide();
+            $('.customerOrder').hide();
+
         });
     }).run();    
 };
